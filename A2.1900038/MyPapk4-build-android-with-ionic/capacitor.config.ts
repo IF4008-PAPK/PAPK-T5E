30fd64aad403cbf7db1e444f96d342ee12e5c7c8
A21900038-DedeSupriatnaa <a2.1900038@mhs.stmik-sumedang.ac.id>
2021-11-09 21:39:44 +0700

    MyPapk4-Build-Android-APP-with-Ionic

---
+++ A2.1900038/MyPapk4-build-android-with-ionic/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyPapk4-build-android-with-ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
